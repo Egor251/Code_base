5415ef26f2046e0b1b5a07121ec57654d52c517e
Egor251 <grimmallod@mail.ru>
2024-05-19 22:25:43 +0300

    minor fix

--- xslx/xlsx_read.py
+++ xslx/xlsx_read.py
@@ -4,8 +4,8 @@ def read_xlsx(file_path):
     output_list = []  # Выходной список
     rb = xlrd.open_workbook(file_path)  # Читаем xlsx-файл
     sheet = rb.sheet_by_index(0)  # Указываем нужный лист
-    for rownum in range(sheet.nrows):  # Считываем файл построчно
-        output_list.append(sheet.row_values(rownum))  # Вносим в выходной список
+    for row_num in range(sheet.nrows):  # Считываем файл построчно
+        output_list.append(sheet.row_values(row_num))  # Вносим в выходной список
 
     return output_list  # Возвращаем выходной список
 
